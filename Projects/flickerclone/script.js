a862c4be9b7b64d296dbe449e58d808ae0d48a51
Saoud Ahmed <135643500+Saoud-Ahmed@users.noreply.github.com>
2023-08-31 18:00:50 +0500

    flickr clone

--- Projects/flickerclone/script.js
+++ Projects/flickerclone/script.js
@@ -13,3 +13,108 @@ function updateInputWidth() {
 // Attach the function to the window's resize event
 window.addEventListener('resize', updateInputWidth);
 
+
+const arraypics=[
+    {
+        "image":"images/bg1.jpg",
+        "text":"Breathtaking Pictures",
+        "author":"by random artists"
+    },
+    {
+        "image":"images/bg2.jpg",
+        "text":"Breathtaking Pictures",
+        "author":"by random artists"
+    },
+    {
+        "image":"images/bg3.jpg",
+        "text":"Breathtaking Pictures",
+        "author":"by random artists"
+    },
+    {
+        "image":"images/bg4.jpg",
+        "text":"Breathtaking Pictures",
+        "author":"by random artists"
+    },
+    {
+        "image":"images/bg5.jpg",
+        "text":"Breathtaking Pictures",
+        "author":"by random artists"
+    },
+    {
+        "image":"images/bg6.jpg",
+        "text":"Breathtaking Pictures",
+        "author":"by random artists"
+    },
+    {
+        "image":"images/bg7.jpg",
+        "text":"Breathtaking Pictures",
+        "author":"by random artists"
+    },
+    {
+        "image":"images/bg8.jpg",
+        "text":"Breathtaking Pictures",
+        "author":"by random artists"
+    },
+    {
+        "image":"images/bg9.jpg",
+        "text":"Breathtaking Pictures",
+        "author":"by random artists"
+    }
+]
+
+
+const altText=document.querySelector(".alttext");
+const altText2=document.querySelector(".alttext2");
+const contentContainer = document.getElementById('contentcontainer');
+let currentIndex = 0; // Keep track of the current image index
+
+
+
+function updateContent(currentIndex) {
+
+    const imgElement = document.createElement('img');
+    
+
+    imgElement.src = arraypics[currentIndex].image;
+    imgElement.alt = arraypics[currentIndex].text;
+    altText.textContent=arraypics[currentIndex].text;
+    altText2.textContent=arraypics[currentIndex].author;
+  
+    imgElement.classList.add('slide-from-top');
+
+
+    contentContainer.innerHTML = ''; // Clear existing content
+    contentContainer.appendChild(imgElement);
+
+
+    imgElement.offsetWidth;
+
+    // Remove the slide-from-top class to trigger the transition
+    imgElement.classList.remove('slide-from-top');
+
+    // Increment the index or reset to 0 if at the end of the array
+    currentIndex = (currentIndex + 1) % arraypics.length;
+}
+
+updateContent(currentIndex);
+
+
+const interval = setInterval(function(){updateContent(currentIndex);}, 5000); // 10 seconds in milliseconds
+
+let logo=document.querySelector(".navbar-brand");
+
+
+logo.addEventListener("click", function() {
+    clearInterval(interval); // Clear the existing interval
+    let extra = Math.floor(Math.random() * 10);
+    if(extra == currentIndex) {
+        extra = Math.floor(Math.random() * 10);
+        currentIndex = extra;
+        updateContent(currentIndex);
+    }
+    else{
+        currentIndex = extra;
+        updateContent(currentIndex);
+    }
+});
+
